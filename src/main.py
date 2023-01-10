6591be90f622f5f9265fdf7065fa49df176b62cb
pigozzif <pigozzife@gmail.com>
2023-01-10 13:08:53 +0100

    fix file name

--- src/main.py
+++ src/main.py
@@ -108,10 +108,10 @@ if __name__ == "__main__":
     set_seed(arguments.seed)
     seed = arguments.seed
     if arguments.visualize == 1:
-        listener = VizListener(file_path=".".join([str(arguments.solver), str(seed), str(arguments.problem), "txt"]),
+        listener = VizListener(file_path=".".join([str(arguments.clustering), str(seed), str(arguments.problem), "txt"]),
                                header=["iteration", "elapsed.time", "best.fitness", "avg.distance"])
     else:
-        listener = MyListener(file_path=".".join([str(arguments.solver), str(seed), str(arguments.problem), "txt"]),
+        listener = MyListener(file_path=".".join([str(arguments.clustering), str(seed), str(arguments.problem), "txt"]),
                               header=["iteration", "elapsed.time", "best.fitness", "avg.distance"])
     fitness = MyFitness(function=CEC2013(arguments.problem))
     # print(fitness.function.get_info())
