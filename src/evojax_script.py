acac9b2e71e947bddca3dfffe10d41c7a8da0ed9
pigozzif <pigozzife@gmail.com>
2023-01-18 23:14:43 +0100

    import other stuff

--- src/evojax_script.py
+++ src/evojax_script.py
@@ -48,8 +48,7 @@ from evojax import SimManager
 import jax
 import jax.numpy as jnp
 import chex
-from typing import Tuple, Optional, Union
-from evosax.utils import GradientOptimizer, OptState, OptParams, exp_decay
+from typing import Tuple, Optional, Union, Dict
 from flax import struct
 from functools import partial
 from .utils import get_best_fitness_member, ParameterReshaper, FitnessShaper
@@ -237,6 +236,165 @@ class Strategy(object):
         else:
             x_out = state.mean
         return x_out
+
+
+def exp_decay(
+    param: chex.Array, param_decay: chex.Array, param_limit: chex.Array
+) -> chex.Array:
+    """Exponentially decay parameter & clip by minimal value."""
+    param = param * param_decay
+    param = jnp.maximum(param, param_limit)
+    return param
+
+
+@struct.dataclass
+class OptState:
+    lrate: float
+    m: chex.Array
+    v: Optional[chex.Array] = None
+    n: Optional[chex.Array] = None
+    last_grads: Optional[chex.Array] = None
+    gen_counter: int = 0
+
+
+@struct.dataclass
+class OptParams:
+    lrate_init: float = 0.01
+    lrate_decay: float = 0.999
+    lrate_limit: float = 0.001
+    momentum: Optional[float] = None
+    beta_1: Optional[float] = None
+    beta_2: Optional[float] = None
+    beta_3: Optional[float] = None
+    eps: Optional[float] = None
+    max_speed: Optional[float] = None
+
+
+class Optimizer(object):
+    def __init__(self, num_dims: int):
+        """Simple JAX-Compatible Optimizer Class."""
+        self.num_dims = num_dims
+
+    @property
+    def default_params(self) -> OptParams:
+        """Return shared and optimizer-specific default parameters."""
+        return OptParams(**self.params_opt)
+
+    def initialize(self, params: OptParams) -> OptState:
+        """Initialize the optimizer state."""
+        return self.initialize_opt(params)
+
+    def step(
+        self,
+        mean: chex.Array,
+        grads: chex.Array,
+        state: OptState,
+        params: OptParams,
+    ) -> [chex.Array, OptState]:
+        """Perform a gradient-based update step."""
+        return self.step_opt(mean, grads, state, params)
+
+    def update(self, state: OptState, params: OptParams) -> OptState:
+        """Exponentially decay the learning rate if desired."""
+        lrate = exp_decay(state.lrate, params.lrate_decay, params.lrate_limit)
+        return state.replace(lrate=lrate)
+
+    @property
+    def params_opt(self) -> OptParams:
+        """Optimizer-specific hyperparameters."""
+        raise NotImplementedError
+
+    def initialize_opt(self, params: OptParams) -> OptState:
+        """Optimizer-specific initialization of optimizer state."""
+        raise NotImplementedError
+
+    def step_opt(
+        self,
+        mean: chex.Array,
+        grads: chex.Array,
+        state: OptState,
+        params: OptParams,
+    ) -> Tuple[chex.Array, OptState]:
+        """Optimizer-specific step to update parameter estimates."""
+        raise NotImplementedError
+
+
+class SGD(Optimizer):
+    def __init__(self, num_dims: int):
+        """Simple JAX-Compatible SGD + Momentum optimizer."""
+        super().__init__(num_dims)
+        self.opt_name = "sgd"
+
+    @property
+    def params_opt(self) -> Dict[str, float]:
+        """Return default SGD+Momentum parameters."""
+        return {
+            "momentum": 0.0,
+        }
+
+    def initialize_opt(self, params: OptParams) -> OptState:
+        """Initialize the momentum trace of the optimizer."""
+        return OptState(m=jnp.zeros(self.num_dims), lrate=params.lrate_init)
+
+    def step_opt(
+        self,
+        mean: chex.Array,
+        grads: chex.Array,
+        state: chex.ArrayTree,
+        params: chex.ArrayTree,
+    ) -> Tuple[chex.Array, OptState]:
+        """Perform a simple SGD + Momentum step."""
+        m = grads + params.momentum * state.m
+        mean_new = mean - state.lrate * state.m
+        return mean_new, state.replace(m=m, gen_counter=state.gen_counter + 1)
+
+
+class Adam(Optimizer):
+    def __init__(self, num_dims: int):
+        """JAX-Compatible Adam Optimizer (Kingma & Ba, 2015)
+        Reference: https://arxiv.org/abs/1412.6980"""
+        super().__init__(num_dims)
+        self.opt_name = "adam"
+
+    @property
+    def params_opt(self) -> Dict[str, float]:
+        """Return default Adam parameters."""
+        return {
+            "beta_1": 0.99,
+            "beta_2": 0.999,
+            "eps": 1e-8,
+        }
+
+    def initialize_opt(self, params: OptParams) -> OptState:
+        """Initialize the m, v trace of the optimizer."""
+        return OptState(
+            m=jnp.zeros(self.num_dims),
+            v=jnp.zeros(self.num_dims),
+            lrate=params.lrate_init,
+        )
+
+    def step_opt(
+        self,
+        mean: chex.Array,
+        grads: chex.Array,
+        state: OptState,
+        params: OptParams,
+    ) -> Tuple[chex.Array, OptState]:
+        """Perform a simple Adam GD step."""
+        m = (1 - params.beta_1) * grads + params.beta_1 * state.m
+        v = (1 - params.beta_2) * (grads ** 2) + params.beta_2 * state.v
+        mhat = m / (1 - params.beta_1 ** (state.gen_counter + 1))
+        vhat = v / (1 - params.beta_2 ** (state.gen_counter + 1))
+        mean_new = mean - state.lrate * mhat / (jnp.sqrt(vhat) + params.eps)
+        return mean_new, state.replace(
+            m=m, v=v, gen_counter=state.gen_counter + 1
+        )
+
+
+GradientOptimizer = {
+    "sgd": SGD,
+    "adam": Adam
+}
 
 
 @struct.dataclass
